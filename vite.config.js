31e67bd44f9eda066976c7860d2fb3a0d5988b49
sudhin4 <sudhinnaveen38@gmail.com>
2025-09-01 23:44:31 +0530

    Image mistake

--- vite.config.js
+++ vite.config.js
@@ -18,8 +18,8 @@ export default defineConfig({
         background_color: '#000000',
         theme_color: '#1d4ed8',
         icons: [
-          { src: 'dollar.png', sizes: '192x192', type: 'image/png' },
-          { src: 'dollar.png', sizes: '512x512', type: 'image/png' },
+          { src: '/dollar.png', sizes: '192x192', type: 'image/png' },
+          { src: '/dollar.png', sizes: '512x512', type: 'image/png' },
         ],
       },
       workbox: {
